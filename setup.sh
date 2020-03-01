66e9575272271e791ed53560e54b0a0ba9b18114
Adam Snaider <adsnaider@gmail.com>
2020-03-01 14:40:44 -0800

    Install gtest.

--- setup.sh
+++ setup.sh
@@ -6,5 +6,13 @@ curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 apt update && apt install bazel -y
 
-bazel build -c opt @stargrade//:runner
+# GTest
+apt install libgtest-dev
+apt install cmake
+cd /usr/src/gtest
+cmake CMakeLists.txt
+make
+cp *.a /usr/lib/
+
 bazel build -c opt //tests
+bazel build -c opt @stargrade//:runner
