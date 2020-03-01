7ab082ee705fca2eac98fa894f8b51561f4b52f8
Adam Snaider <adsnaider@gmail.com>
2020-03-01 12:20:35 -0800

    apt-get should always be run with -y

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Bazel
-apt install curl
+apt install curl -y
 curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt update && apt install bazel
+apt update && apt install bazel -y
